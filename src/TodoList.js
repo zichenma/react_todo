1672cbac7b67c2ebab1cdc625773f1c34be218fe
Liepin-NorthAmerica <nini10171017>
2019-04-13 18:02:43 -0400

    finished 5_5

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,13 +9,38 @@ class TodoList extends React.Component {
     state = store.getState();
     constructor(props) {
         super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+    handleInputChange (e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+    handleStoreChange () {
+        this.setState(store.getState());
+    }
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        }
+        store.dispatch(action);
     }
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft:'10px'}}> 
                 <div>
-                    <Input value={this.state.inputValue} placeholder="todo info" style={{width: '300px', marginRight: '10px'}}/>
-                    <Button type="primary">Submit</Button>
+                    <Input 
+                    value={this.state.inputValue} 
+                    placeholder="todo info" 
+                    style={{width: '300px', marginRight: '10px'}}
+                    onChange={this.handleInputChange}
+                    />
+                    <Button onClick={this.handleBtnClick} type="primary">Submit</Button>
                 </div>
                 <List style={{marginTop: '10px', width: '300px'}}
                     bordered
@@ -25,6 +50,7 @@ class TodoList extends React.Component {
             </div>
         )
     }
+
 }
 
 export default TodoList;
