c2bb6eb16a633629e10659f4250a9f0b391a252b
Liepin-NorthAmerica <nini10171017>
2019-04-16 13:45:37 -0400

    change to spread

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -19,7 +19,7 @@ export default (state = defaultState, action) => {
     }
     if (action.type === INIT_LIST_ACTION) {
         const newState = JSON.parse(JSON.stringify(state));
-        newState.list = action.data;
+        newState.list = [...action.data];
         return newState;
     }
     if (action.type === ADD_TODO_ITEM) {
