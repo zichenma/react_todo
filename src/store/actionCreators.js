8fa1ab11fb900ea49401bf9484106066df8ae42e
Liepin-NorthAmerica <nini10171017>
2019-04-16 14:53:32 -0400

    console.error

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -24,7 +24,7 @@ export const initListAction = data => ({
 export const getTodoList = () => {
     return async (dispatch) => {
         const uri = `http://localhost:3200/`;
-        const response = await axios.get(`${uri}todos`).catch(err => console.log(err));
+        const response = await axios.get(`${uri}todos`).catch(err => console.error(err));
         const data = response.data;
         const action = initListAction(data);
         dispatch(action);
