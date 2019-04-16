a7c53d849e2c783c9038d485de4042252e823595
Liepin-NorthAmerica <nini10171017>
2019-04-15 20:07:38 -0400

    finished 6_1

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import { Input, Button, List, Typography } from 'antd';
+
+
 // 自动默认去找index.js
 import store from './store';
 import { getInputChangeAction, getAddItemAction, getHandleItemDelete } from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 class TodoList extends React.Component {
     state = store.getState();
@@ -12,6 +13,7 @@ class TodoList extends React.Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleStoreChange = this.handleStoreChange.bind(this);
         this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
         store.subscribe(this.handleStoreChange);
     }
     handleInputChange (e) {
@@ -30,25 +32,13 @@ class TodoList extends React.Component {
         store.dispatch(action);
     }
     render() {
-        return (
-            <div style={{marginTop: '10px', marginLeft:'10px'}}> 
-                <div>
-                    <Input 
-                    value={this.state.inputValue} 
-                    placeholder="todo info" 
-                    style={{width: '300px', marginRight: '10px'}}
-                    onChange={this.handleInputChange}
-                    />
-                    <Button onClick={this.handleBtnClick} type="primary">Submit</Button>
-                </div>
-                <List style={{marginTop: '10px', width: '300px'}}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={(item, index) => (<List.Item><Typography.Text mark></Typography.Text> {item} 
-                    <button type="primary" onClick={this.handleItemDelete.bind(this, index)} style={{marginLeft: '10px'}}>Delete</button></List.Item>)}
-                />
-            </div>
-        )
+        return <TodoListUI 
+            inputValue={this.state.inputValue}
+            list={this.state.list}
+            handleInputChange={this.handleInputChange}
+            handleBtnClick={this.handleBtnClick}
+            handleItemDelete={this.handleItemDelete}
+        />;
     }
 
 }
